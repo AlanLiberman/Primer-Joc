3d4a7ae61698f99674edcc7faeebfe27be637054
AlanLiberman <alanliberman11@gmail.com>
2021-03-26 14:39:35 +0100

    The true start of the game
    parkour (has short cuts), new animation.  Lifepoints, Deathzone , respawn point added

--- Player.gd
+++ Player.gd
@@ -1,19 +1,32 @@
 extends KinematicBody2D
 
 var velocity : Vector2
+var life = 1000
 	
 func _process(delta):
 	velocity.x = 0
 	$AnimatedSprite.flip_v = false
 	if Input.is_action_pressed("right"):
-		velocity += Vector2.RIGHT * 300
+		velocity += Vector2.RIGHT * 100
 		$AnimatedSprite.flip_h = false
 	if Input.is_action_pressed("left"):
+		velocity += Vector2.LEFT * 100
+		$AnimatedSprite.flip_h = true
+	if Input.is_action_pressed("right") and Input.is_action_pressed("sprint"):
+		velocity += Vector2.RIGHT * 300
+		$AnimatedSprite.flip_h = false
+	if Input.is_action_pressed("left") and Input.is_action_pressed("sprint"):
 		velocity += Vector2.LEFT * 300
 		$AnimatedSprite.flip_h = true
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		velocity.y += -200
-			
+		velocity.y += -250
+		
+	if position.y >= 1500:
+		life -= 1000
+	if life <= 0:
+		position = Vector2(25,490)
+		life = 1000
+
 	velocity.y += 200 * delta
 			
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -27,3 +40,10 @@ func animate(velocity:Vector2):
 		$AnimatedSprite.play("walk")
 	if velocity.x < 0:
 		$AnimatedSprite.play("walk")
+	if velocity.x > 100:
+		$AnimatedSprite.play("run")
+	if velocity.x < -100:
+		$AnimatedSprite.play("run")
+	if velocity.y > 0:
+		$AnimatedSprite.play("fall")
+
