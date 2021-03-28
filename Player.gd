bdb31c439070463b49f9419b6cf5a398043e7bec
AlanLiberman <alanliberman11@gmail.com>
2021-03-28 03:49:22 +0200

    added enemy
    added enemy, new parkour, bullets

--- Player.gd
+++ Player.gd
@@ -6,17 +6,17 @@ var life = 1000
 func _process(delta):
 	velocity.x = 0
 	$AnimatedSprite.flip_v = false
-	if Input.is_action_pressed("right"):
-		velocity += Vector2.RIGHT * 100
-		$AnimatedSprite.flip_h = false
-	if Input.is_action_pressed("left"):
-		velocity += Vector2.LEFT * 100
-		$AnimatedSprite.flip_h = true
 	if Input.is_action_pressed("right") and Input.is_action_pressed("sprint"):
 		velocity += Vector2.RIGHT * 300
 		$AnimatedSprite.flip_h = false
+	elif Input.is_action_pressed("right"):
+		velocity += Vector2.RIGHT * 100
+		$AnimatedSprite.flip_h = false
 	if Input.is_action_pressed("left") and Input.is_action_pressed("sprint"):
 		velocity += Vector2.LEFT * 300
+		$AnimatedSprite.flip_h = true
+	elif Input.is_action_pressed("left"):
+		velocity += Vector2.LEFT * 100
 		$AnimatedSprite.flip_h = true
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y += -250
---
+++ enemy.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+var ball
+var timer = 0.0
+
+func _ready():
+	ball = $Sprite
+	$AnimatedSprite.play("standby")
+	ball.visible = false
+
+func _process(delta):
+	timer += 10
+
+func _on_Area2D_body_entered(body):
+	timer = 0
+	ball.visible = true
+	$AnimatedSprite.play("attack")
+	print('why')
+	#if timer < 1:
+	#	ball.visible = true
+	#	ball.position += 100
+	#else:
+	#	ball.visible = false
+	#	ball.position == Vector2(0,0)
+
+func _on_Area2D_body_exited(body):
+	$AnimatedSprite.play("standby")
+	ball.visible = false
+
